e46f59a15dbca7772cc826f98baea148c560e0aa
Eetu Knutars <96994953+knuutti@users.noreply.github.com>
2024-12-05 17:00:39 +0200

    Removed tests

--- digit_classify.py
+++ digit_classify.py
@@ -1,7 +1,6 @@
 import classifier as c
 import torch
 import numpy as np
-import pandas as pd
 
 # Classifier function, takes Nx3 matrix as an 
 # input and returns an integer from range 0-9
@@ -16,20 +15,3 @@ def digit_classify(data):
     features = torch.tensor(np.matrix(features)).float()
     output = model(features)
     return torch.argmax(output).item()
-
-
-if __name__ == "__main__":
-
-    # Testing with all data samples
-    correct = 0
-    print("Starting to classify...")
-    for num in range(10):
-        for sample in range(1,101):
-            fname = f'data/stroke_{num}_{str(0)*(4-len(str(sample))) + str(sample)}.csv'
-            testdata = pd.read_csv(fname).to_numpy()
-            print(np.shape(testdata))
-            predicted = digit_classify(testdata)
-            if num == predicted:
-                correct += 1
-    print(f"Accuracy: {correct/1000:.2f}")
-
